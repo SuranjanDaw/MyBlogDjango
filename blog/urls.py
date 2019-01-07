49e5ced41320ef6e94227174afb34bcb1f1f6755
SuranjanDaw <suranjandaw@gmail.com>
2019-01-07 20:26:26 +0530

    4th commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list,name='post_list')
+    path('post1',views.post_list,name='post_list'),
+    path('post2/<int:pk>/',views.post_details,name='post_details')
 ]
