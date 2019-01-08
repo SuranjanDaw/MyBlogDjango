08bdaf63fe2e5dcbcae16c973c8b7479aecdd175
SuranjanDaw <suranjandaw@gmail.com>
2019-01-09 03:00:00 +0530

    UI changes

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post1',views.post_list,name='post_list'),
-    path('post2/<int:pk>/',views.post_details,name='post_details')
+    path('',views.post_list,name='post_list'),
+    path('post/<int:pk>/',views.post_details,name='post_details')
 ]
